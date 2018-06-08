515bf4988f7cdf164a7cb64b6bbd1a6a9b2c248b
Colin Yau <yaucolin@yahoo.com>
2018-06-08 16:29:49 -0400

    Changed WordPress theme from twentyfourteen to twentysixteen.

---
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function wpb_add_google_fonts() {
+
+	wp_enqueue_style( 'wpb-google-fonts',
+		'https://fonts.googleapis.com/css?family=Sunflower',
+		false );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+function my_theme_enqueue_styles() {
+
+	$parent_style = 'twentysixteen-style'; //This is 'twentysixteen-style' for the Twenty Sixteen theme.
+
+	wp_enqueue_style( $parent_style,
+		get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get('Version')
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
